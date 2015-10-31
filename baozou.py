e5577d9e97a0038e27a7752af4f894c3d3964e4b
zhuhui <zhuhui_ss@163.com>
2015-11-01 00:25:14 +0800

    commet done

--- baozou.py
+++ baozou.py
@@ -1,4 +1,5 @@
 import requests
+import copy
 
 g_client_id = "10230158"
 g_secret_key = "18a75cf12dff8cf6e17550e25c860839"
@@ -68,14 +69,14 @@ def article_up(article, user):
 def get_detail(user):
     """
     :param user:  class:`BZUser` object and i need `uid` and `token`
-    :return: (bool, reponse)
+    :return: (bool, class:`Response` object)
     ### this function will update and fill the user ###
     """
     post_map = {}
     post_map["client_id"] = g_client_id
-    post_map["access_token"] = "4043182e9076f4ba5c283fc73da5cab908f230a2"
-    post_map["id"] = "27823699"
-    post_map["user_id"] = "27823699"
+    post_map["access_token"] = user.token
+    post_map["id"] = user.uid
+    post_map["user_id"] = user.uid
     post_map["timestamp"] = str(curtimespan())
     post_map["sign"] = get_sign(post_map)
     url = "http://api.ibaozou.com/api/v2/" + "users/" + "27823699" + "?" + create_post_string(post_map)
@@ -92,6 +93,37 @@ def get_detail(user):
         pass
 
     return (success, resp,)
+
+def article_commet(user, article, content):
+    """
+    :param user: class:`BZUser` object and i need `uid` and `token`
+    :param article: the articleID
+    :param content: the text what you want to say
+    :return: (bool, class:`Response` object)
+    """
+    post_map = {}
+    post_map["client_id"] = g_client_id
+    post_map["user_id"] = user.uid
+    post_map["access_token"] = user.token
+    post_map["anonymous"] = "false"
+    post_map["content"] = content
+    post_map["timestamp"] = str(curtimespan())
+    tmp_map = copy.copy(post_map)
+    tmp_map["id"]=article
+    post_map["sign"] = get_sign(tmp_map)
+
+    url = "http://api.ibaozou.com/api/v1/articles/" + article + "/comments"
+
+    success = False
+    resp = None
+    try:
+        resp = requests.post(url, post_map)
+        if 'error' not in resp.text:
+            success = True
+    except Exception, e:
+        pass
+
+    return success, resp
 
 def create_post_string(dict):
     res = []
